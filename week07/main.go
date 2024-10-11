8853ff0e581351efab619cf09bd4996473fd22cf
shj02 <shj507805@naver.com>
2024-10-11 18:34:47 +0900

    v1.3 Scope

--- week07/main.go
+++ week07/main.go
@@ -18,10 +18,13 @@ func main() {
 	}
 
 	score = strings.TrimSpace(score)                // TrimSpace() 공백, 띄어쓰기, 줄바꿈 제거(python의 strip과 유사)
-	realScore, _ := strconv.ParseInt(score, 16, 32) // 정수형 32비트 타입으로 형변환(string, 진법, 비트), := 선언 단축
-	if realScore >= 60 {
-		fmt.Println("A")
+	realScore, _ := strconv.ParseInt(score, 10, 32) // 정수형 32비트 타입으로 형변환(string, 진법, 비트), := 선언 단축
+
+	var grade string // 선언
+	if realScore >= 90 {
+		grade = "A" // := 쓰면 안됨! 전역변수와 같다고 표현
 	} else {
-		fmt.Println("BCDF")
+		grade = "BCDF"
 	}
+	fmt.Printf("%d점은 %s등급입니다.\n", realScore, grade)
 }
