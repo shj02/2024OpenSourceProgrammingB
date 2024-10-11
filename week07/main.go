e36c8013596903e07f4f8305ebc2143a133780a7
shj02 <shj507805@naver.com>
2024-10-11 18:27:00 +0900

    v1.3 ParseInt, base16

--- week07/main.go
+++ week07/main.go
@@ -18,7 +18,7 @@ func main() {
 	}
 
 	score = strings.TrimSpace(score)                // TrimSpace() 공백, 띄어쓰기, 줄바꿈 제거(python의 strip과 유사)
-	realScore, _ := strconv.ParseInt(score, 10, 32) // 정수형 32비트 타입으로 형변환(string, 진법, 비트), := 선언 단축
+	realScore, _ := strconv.ParseInt(score, 16, 32) // 정수형 32비트 타입으로 형변환(string, 진법, 비트), := 선언 단축
 	if realScore >= 60 {
 		fmt.Println("A")
 	} else {
