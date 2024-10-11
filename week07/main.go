80ebc80b05cab7987eb56ac1d7943fbabc731df9
shj02 <shj507805@naver.com>
2024-10-11 16:57:52 +0900

    v0.8 Replace

--- week07/main.go
+++ week07/main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"strings"
 )
 
 func main() {
-	var now time.Time = time.Now()
-	fmt.Printf("오늘은 %d년 %d월 %d일\n", now.Year(), int(now.Month()), now.Day())
-	fmt.Printf("지금은 %d시 %d분 %d초", now.Hour(), now.Minute(), now.Second())
+	var army string = "우리는 !가와 !민에 충성을 다하는 대한민! 육군이다."
+	armyFixed := strings.NewReplacer("!", "국")
+	fmt.Println(army)
+	fmt.Println(armyFixed.Replace(army))
 }
