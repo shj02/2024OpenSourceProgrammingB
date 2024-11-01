f0ff0a90b4ac31873748a6e8dfdbfb21dc29cbe4
shj02 <shj507805@naver.com>
2024-11-01 18:08:53 +0900

    v2.8 Prime number

--- week10/main.go
+++ week10/main.go
@@ -10,7 +10,6 @@ import (
 )
 
 func main() {
-	// fmt.Printf("%f\n", math.Sqrt(25.0))
 	fmt.Printf("정수 입력 :")
 	in := bufio.NewReader(os.Stdin)
 	number, err := in.ReadString('\n')
@@ -25,18 +24,22 @@ func main() {
 	}
 
 	var isPrime bool = true
-	if n <= 1 {
+	if n <= 1 { // 1 false
 		isPrime = false
-	} else {
-		i := 2
-		// for i < n {
+	} else if n == 2 { // 2 true
+		isPrime = true
+	} else if n%2 == 0 { // 2를 제외한 모든 짝수는 소수가 아님
+		isPrime = false
+	} else { // 2보다 큰 홀수
+		i := 3
 		for i <= int(math.Sqrt(float64(n))) {
 			if n%i == 0 {
 				isPrime = false
-				break // 1과 자기자신을 제외한 첫 번째 약수가 발견되면 반복문 종료
+				break
 			}
-			fmt.Printf("%d ", i) // 반복 횟수 확인용 코드
-			i++
+			fmt.Printf("%d ", i)
+			// i++
+			i = i + 2
 		}
 	}
 
