111ebe4a79127a823069b474bba199dd29ab1bff
shj02 <shj507805@naver.com>
2024-11-01 17:14:17 +0900

    v2.5 Prime number

--- week10/main.go
+++ week10/main.go
@@ -22,21 +22,25 @@ func main() {
 		fmt.Println(err)
 	}
 
-	count := 0
+	// count := 0
 
+	var isPrime bool = true // int -> bool, count -> isPrime, 메모리가 적음
 	if n <= 1 {
-		count = -1
+		// count = -1
+		isPrime = false // 가독성
 	} else {
 		i := 2
 		for i < n {
 			if n%i == 0 {
-				count = count + 1 // count++
+				// count = count + 1
+				isPrime = false // +연산 제거
 			}
 			i++
 		}
 	}
 
-	if count == 0 {
+	// if count == 0
+	if isPrime { // 비교연산 제거
 		fmt.Printf("%d는(은) 소수입니다", n)
 	} else {
 		fmt.Printf("%d는(은) 소수가 아닙니다", n)
