8bfdb6cde362292f11b1ced8056c8a856180d31b
shj02 <shj507805@naver.com>
2024-11-15 17:11:40 +0900

    v4.0 Array

--- week12/main.go
+++ week12/main.go
@@ -28,7 +28,15 @@ func main() {
 		time.Unix(0, 0),
 		time.Unix(1, 0),
 		time.Unix(1708012346, 0)} // 중괄호랑 이어져 있으면 콤마 안 써도 됨!
-	fmt.Println(dates[0], dates[1], dates[2]) // unix time, zero value, 2024-02-16 ...
-	fmt.Println(dates)                        // array
-	fmt.Printf("%#v\n", dates)                // array literal
+	// fmt.Println(dates[0], dates[1], dates[2]) // unix time, zero value, 2024-02-16 ...
+	// fmt.Println(dates)                        // array
+	// fmt.Printf("%#v\n", dates)                // array literal
+
+	// for i, date := range dates { // = python 'for in'
+	// 	fmt.Println(i, date)
+	// }
+
+	for _, date := range dates { // = python 'for in', i를 _로 바꾸어서 인덱스는 출력 안되게 함
+		fmt.Println(date)
+	}
 }
