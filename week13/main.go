767b091adfdd76d8b3cbdc2f8ff064bd2eb61bcd
shj02 <shj507805@naver.com>
2024-11-22 16:59:58 +0900

    v4.3 Slice

--- week13/main.go
+++ week13/main.go
@@ -3,6 +3,10 @@ package main
 import "fmt"
 
 func main() {
+	var emptySlice []bool
+	emptySlice = make([]bool, 5)
+	fmt.Printf("%#v\n", emptySlice) //  slice zero value : (nil)
+
 	gpas := [5]float64{3.5, 4.1, 4.5, 3.9, 4.23}
 	gpa_slice := gpas[1:4] // [4.1, 4.5, 3.9]
 	// gpa_slice[1] = 2.71
@@ -10,5 +14,4 @@ func main() {
 	// gpa_slice = append(gpa_slice, 4.3)
 	gpa_slice = append(gpa_slice, 4.3, 5.55)
 	fmt.Println(len(gpa_slice), gpa_slice, gpas)
-
 }
