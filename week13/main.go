eba07007e16bf84f3399329d244b62b3f8eeedf2
shj02 <shj507805@naver.com>
2024-11-23 14:12:44 +0900

    v4.5 Variable Argument

--- week13/main.go
+++ week13/main.go
@@ -1,21 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"reflect"
+)
+
+// func test(strs string) {
+// func test(strs ...string, i int) {   // error
+func test(i int, strs ...string) { /// ... : 가변매개변수
+	fmt.Println(i, strs, reflect.TypeOf(strs))
+}
 
 func main() {
-	var emptySlice []bool
-	// emptySlice = make([]bool, 5)
-	fmt.Printf("%#v %d\n", emptySlice, len(emptySlice)) //  slice zero value : (nil)
-	if len(emptySlice) == 0 {
-		emptySlice = append(emptySlice, true)
-	}
-	fmt.Printf("%#v %d\n", emptySlice, len(emptySlice)) // []bool(true), 1
-
-	gpas := [5]float64{3.5, 4.1, 4.5, 3.9, 4.23}
-	gpa_slice := gpas[1:4] // [4.1, 4.5, 3.9]
-	// gpa_slice[1] = 2.71
-	gpas[2] = 2.71
-	// gpa_slice = append(gpa_slice, 4.3)
-	gpa_slice = append(gpa_slice, 4.3, 5.55)
-	fmt.Println(len(gpa_slice), gpa_slice, gpas)
+	// fmt.Println(os.Args, len(os.Args), reflect.TypeOf(os.Args))
+	slices := os.Args[1:] // 0번방: 실행경로
+	fmt.Println(slices, slices[2])
+	test(1, "123")
+	test(-99, "123", "abc")
+	test(55)
+	test(0, "123", "abc", "123a")
 }
