1043241d3b6972007c089f76fdd889028d157933
shj02 <shj507805@naver.com>
2024-11-29 17:46:28 +0900

    v4.7 Struct

--- week14/main.go
+++ week14/main.go
@@ -3,23 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	ages := make(map[string]int)
-
-	var name string
-	var age int
-
-	for { // 무한루프
-		fmt.Print("Name? ")
-		fmt.Scanln(&name)
-		if name == "q" {
-			break
-		}
-		fmt.Print("Age? ")
-		fmt.Scanln(&age)
-
-		ages[name] = age // ages map에 이름과 나이를 넣음
+	var student1 struct { // 구조체
+		id   int
+		name string
+		gpa  float32
 	}
-	for k, v := range ages {
-		fmt.Printf("%s is %d years old~\n", k, v)
+	student1.id = 202444040
+	student1.name = "Son Hyeji"
+	student1.gpa = 4.5
+	fmt.Println(student1.gpa)
+
+	var student2 struct {
+		id   int
+		name string
+		gpa  float32
 	}
+	student2.id = 202444040
+	student2.name = "Son Hyeji"
+	student2.gpa = 4.5
+	fmt.Println(student2.id)
 }
