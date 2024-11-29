eb73ce64d941aec28189423454b456266ef6722a
shj02 <shj507805@naver.com>
2024-11-29 17:49:27 +0900

    v4.8 Struct, Type

--- week14/main.go
+++ week14/main.go
@@ -2,24 +2,22 @@ package main
 
 import "fmt"
 
+type student struct {
+	id   int
+	name string
+	gpa  float32
+}
+
 func main() {
-	var student1 struct { // 구조체
-		id   int
-		name string
-		gpa  float32
-	}
+	var student1 student
 	student1.id = 202444040
 	student1.name = "Son Hyeji"
 	student1.gpa = 4.5
 	fmt.Println(student1.gpa)
 
-	var student2 struct {
-		id   int
-		name string
-		gpa  float32
-	}
-	student2.id = 202444040
-	student2.name = "Son Hyeji"
+	var student2 student
+	student2.id = 20241234
+	student2.name = "Son jihye"
 	student2.gpa = 4.5
 	fmt.Println(student2.id)
 }
